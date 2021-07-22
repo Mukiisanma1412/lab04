83984fb7570a005f55cee0247f3edaa4280bb0bc
Mukiisanma1412 <86796241+Mukiisanma1412@users.noreply.github.com>
2021-07-22 23:53:03 +0700

    1 all

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,10 +1,13 @@
 <template>
-<router-link class="event-link" :to="{name: 'EventDetails', params: {id: event.id}}">
-  <div class="event-card">
-    <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
-  </div>
-</router-link>
+  <router-link
+    class="event-link"
+    :to="{ name: 'EventDetails', params: { id: event.id } }"
+  >
+    <div class="event-card">
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
 </template>
 
 <script>
@@ -32,7 +35,7 @@ export default {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
-.event-link{
+.event-link {
   color: #2c3e50;
   text-decoration: none;
 }
--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,27 +1,29 @@
+
 <template>
-    <div v-if="event"> 
-        <h1> {{ event.tittle}} </h1>
-        <p> {{ event.time }} on {{ event.date }} @ {{ event.location }} </p>
-        <p> {{ event.description }} </p>
-    </div>
+  <div v-if="event">
+    <h1>{{ event.tittle }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['id'],
-        data() {
-            return {
-                event: null,
-            }
-        },
-        created() {
-            EventService.getEvent(this.id)
-            .then(response => {
-                this.event = response.data
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        }
+import EventService from  '@/services/EventService.js'
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null
     }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
 </script>
