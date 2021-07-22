3433c681b1b323d477593c91edc481fac7dd8bb3
Mukiisanma1412 <86796241+Mukiisanma1412@users.noreply.github.com>
2021-07-22 22:46:27 +0700

    1.9 bugged

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import About from '../views/About.vue'
-import EventDetails from '../views/EventDetails.vue'
+import EventDetails from '@/views/EventDetails.vue'
 const routes = [
   {
     path: '/',
@@ -12,7 +12,8 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
-  },{
+  },
+  {
     path: '/event/:id',
     name: 'EventDetails',
     component: EventDetails,
--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -12,7 +12,6 @@
         data() {
             return {
                 event: null,
-            
             }
         },
         created() {
